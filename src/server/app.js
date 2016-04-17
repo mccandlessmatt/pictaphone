6a6b07778a75a8082b1e9340ea83833d238b36b2
Matt McCandless <mmccandl83@gmail.com>
2016-04-16 22:24:12 -0500

    setting up html skeleton

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
 const app = express();
 
+app.use(express.static(`${process.cwd()}/dist`));
 app.listen(process.env.port);
 console.log(`listening on port ${process.env.port}`)

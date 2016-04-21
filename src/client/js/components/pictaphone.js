f56545de6130cedb5fb8f4406ef31ad24d53bc3f
Matt McCandless <mmccandl83@gmail.com>
2016-04-20 21:57:50 -0500

    starting to add state management

--- src/client/js/components/pictaphone.js
+++ src/client/js/components/pictaphone.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Login from './login'
 import Layout from './layout/desktop'
+import { Provider } from 'react-router'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 
-export default props => (
-  <Router history={browserHistory}>
-    <Route path="/" component={Layout}>
-      <IndexRoute component={Login} />
-    </Route>
-  </Router>
+export default ({store}) => (
+  <Provider store = {store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Layout}>
+        <IndexRoute component={Login} />
+      </Route>
+    </Router>
+  </Provider>
 );
--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Pictaphone from './components/pictaphone'
+import { createStore } from 'redux'
 
 const pictaphoneNode = document.getElementById('pictaphone');
+const store = createStore((state, action) => state, {});
 
-ReactDOM.render(<Pictaphone />, pictaphoneNode);
+ReactDOM.render(<Pictaphone store={store} />, pictaphoneNode);

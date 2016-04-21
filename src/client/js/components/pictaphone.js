46eb786a52630cb7d7d045df24a0d8dcb76d4faf
Matt McCandless <mmccandl83@gmail.com>
2016-04-21 17:35:55 -0500

    fixing trying to import Provider from incorrect module

--- src/client/js/components/pictaphone.js
+++ src/client/js/components/pictaphone.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Login from './login'
 import Layout from './layout/desktop'
-import { Provider } from 'react-router'
+import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-
 export default ({store}) => (
-  <Provider store = {store}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={Layout}>
         <IndexRoute component={Login} />

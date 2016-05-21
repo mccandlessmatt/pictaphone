67edc1dc14c8185b3fed624b95b7601193b7a4b4
Matt McCandless <mmccandl83@gmail.com>
2016-05-21 14:25:09 -0500

    starting to add authentication on login

--- src/server/services/authentication.js
+++ src/server/services/authentication.js
@@ -2,15 +2,30 @@ import jwt from 'jsonwebtoken';
 import Users from '../schemas/users';
 import credential from 'credential';
 
+const pw = credential();
+
+function validateUser(req, res, err, user) {
+  if (user) {
+    pw.verify(user.password, req.body.password, (err, isvalid) => {
+      if (isvalid) {
+        const token = jwt.sign({ username: user.email }, process.env.secret);
+        res.json({ token });
+      } else {
+        res.send('incorrect password');
+      }
+    });
+  } else {
+    res.send('can\'t find user');
+  }
+}
+
 function login(req, res) {
-  console.log(`username is ${req.body.username}`);
-  console.log(`password is ${req.body.password}`);
-  const token = jwt.sign({ username: req.body.username }, process.env.secret);
-  res.json({ token });
+  Users
+    .findOne({ email: req.body.username })
+    .exec(validateUser.bind(null, req, res));
 }
 
 function saveUser(userData, res) {
-  console.log('saving hash', typeof userData.hash);
   new Users(userData).save((err, user) => {
     if (err) {
       res.send('error');
@@ -22,7 +37,7 @@ function saveUser(userData, res) {
 function register(req, res) {
   const { name, password, email } = req.body;
 
-  credential().hash(password, (err, hash) => {
+  pw.hash(password, (err, hash) => {
     if (err) {
       res.send('error');
     } else {

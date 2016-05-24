495f364f8c209f9785c253cafc56fbc85ed90d11
Matt McCandless <mmccandl83@gmail.com>
2016-05-23 22:32:07 -0500

    starting to model game data

---
+++ src/server/schemas/game.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema({
+  token: String,
+  pictures: [{ type: Schema.Types.ObjectId, ref: 'Picture' }]
+});
+
+export default mongoose.model('Game', userSchema);
